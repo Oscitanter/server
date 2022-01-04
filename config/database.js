1c6faa3201b0b1e2a40a490407f3c2da203d5ed1
Oscitanter <93686704+Oscitanter@users.noreply.github.com>
2022-01-04 14:47:30 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose')
 
+
+connect("mongodb://localhost/users", {useNewUrlParser: true});
+connection
+    .once('open', () => console.log('Connected'))
+    .on('error', () => {
+        console.log("Your Error", error);
+    });
